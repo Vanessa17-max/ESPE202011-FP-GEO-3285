04a3e2c91ca7a1e75286aa6fca2c26649c73d027
VANESSA PAOLA TONATO TONATO <75495604+Vanessa17-max@users.noreply.github.com>
2021-01-08 15:32:56 -0500

    Add files via upload

---
+++ workshops/unit1/WSO1HelloWorl/ByeWorld.java
@@ -0,0 +1,13 @@
+// Vanessa Tonato Bye World
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class Bey World{
+ public static void main(String[] args){
+ System.out.println("Bey World from Vanessa Tonato");
+ 
+ }
+}
