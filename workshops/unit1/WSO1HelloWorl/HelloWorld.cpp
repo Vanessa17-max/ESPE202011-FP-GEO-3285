8c96997cb9e647a88996b5c210376eff6ef0c403
VANESSA PAOLA TONATO TONATO <75495604+Vanessa17-max@users.noreply.github.com>
2021-01-08 13:54:48 -0500

    Add files via upload

---
+++ workshops/unit1/WSO1HelloWorl/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+ int grade = 20;
+
+ //printf()displays information
+ 
+ printf("Hello World \t\" Vanessa Tonato! \" =) \n");
+ printf (" \n The grade is %d  \n", grade);
+ return 0;
+ 
+ }
---
+++ workshops/unit1/WSO1HelloWorl/HelloWorld.py
@@ -0,0 +1,13 @@
+print("Hello World from Vanessa Tonato \n ESPE \n GEO")
+
+# input variable
+addend1 = input("Enter the first number-->")
+addend2 = input("Enter the first number-->")
+
+# add two numbers
+sum = float(addend1) + float(addend2)
+
+# displaying the sum
+print('the sum of {0} and {1} is {2}'.format(addend1,addend2,sum))
+
+print('the sum is % if' %(float(input('Enter first number: '))+float(input('Enter second number: '))))
