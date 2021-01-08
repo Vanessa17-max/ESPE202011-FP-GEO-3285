93cd4c189404e8462fd8922686e01dae4815f37d
VANESSA PAOLA TONATO TONATO <75495604+Vanessa17-max@users.noreply.github.com>
2021-01-08 15:31:28 -0500

    Add files via upload

---
+++ workshops/unit1/WSO1HelloWorl/HelloWorld.java
@@ -0,0 +1,13 @@
+// Vanessa Tonato Hello World
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class Hello World{
+ public static void main(String[] args){
+ System.out.println("Hello World from Vanessa Tonato");
+ 
+ }
+}
