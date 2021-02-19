ed71788129de97a96ef0a5d4e0e60fab5728072f
VANESSA PAOLA TONATO TONATO <75495604+Vanessa17-max@users.noreply.github.com>
2021-02-19 14:26:58 -0500

    Add files via upload

---
+++ workshops/unit2/WS14_Arrays/littleAnimals/src/littleanimals/LittleAnimals.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package littleanimals;
+
+import java.util.Scanner;
+/**
+ *
+ * @author FAMILIA TONATO
+ */
+public class LittleAnimals {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int [] Animals;
+    String [] little;
+    float [] littleAnimals;
+    
+    Scanner input= new Scanner(System.in); 
+    int n;
+    System.out.println("Enter the name of coops -> ");
+      n = input.nextInt();
+    Animals = new int [n];
+    
+    }
+    
+}
