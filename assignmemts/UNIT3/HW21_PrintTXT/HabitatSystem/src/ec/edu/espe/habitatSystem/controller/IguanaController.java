848a6b6fe55042d8aeb279040df7d83a283bc049
VANESSA PAOLA TONATO TONATO <75495604+Vanessa17-max@users.noreply.github.com>
2021-04-03 11:07:00 -0500

    Add files via upload

---
+++ assignmemts/UNIT3/HW21_PrintTXT/HabitatSystem/src/ec/edu/espe/habitatSystem/controller/IguanaController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.habitatSystem.controller;
+import ec.edu.espe.habitatSystem.model.Iguana;
+import utils.FileManager;
+/**
+ *
+ * @author FAMILIA TONATO
+ */
+public class IguanaController {
+    
+    public void save(Iguana Iguana){
+       String data = Iguana.getSpeciesname() + "," + Iguana.isSpecies() + "," + Iguana.getSkinColor() + "," + Iguana.getAge() + "," + Iguana.getFeeding() + "," + Iguana.getWeight() + "," + Iguana.getLocation() + "," + Iguana.getMoves();
+       FileManager.save(data, "Iguana");
+    }
+}
